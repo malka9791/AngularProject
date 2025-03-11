54f3686ebd12cba9de122c16f170d22f5a7d404c
malka9791 <m0527169791@gmail.com>
2025-03-12 01:29:23 +0200

    try with 3 lesson

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,9 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+// import { RenderMode, ServerRoute } from '@angular/ssr';
 
-export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
-];
+// export const serverRoutes: ServerRoute[] = [
+//   {
+//     path: '**',
+//     renderMode: RenderMode.Prerender,
+//   },
+//   { path: 'lesson/:courseId', renderMode: RenderMode.Normal },
+// ];
