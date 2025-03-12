e5b015308adaceb5b1a2fa9b0f43e4870c663183
malka9791 <m0527169791@gmail.com>
2025-03-12 13:44:46 +0200

    bsd onlyy

--- src/app/guard/add-course.guard.ts
+++ src/app/guard/add-course.guard.ts
@@ -2,8 +2,8 @@ import { CanActivateFn } from '@angular/router';
 
 export const addCourseGuard: CanActivateFn = (route, state) => {
   let role =
-    typeof window !== 'undefined' && typeof localStorage !== 'undefined'
-      ? localStorage.getItem('role')
+    typeof window !== 'undefined' && typeof sessionStorage !== 'undefined'
+      ? sessionStorage.getItem('role')
       : null;
   if (role == 'student') return false;
   return true;
